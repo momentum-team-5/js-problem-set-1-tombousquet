c7e29947a981c09e2284dd5eb1a410f338fc4eec
Tom Bousquet <tom.bousquet@alumni.unc.edu>
2020-09-14 22:49:55 -0400

    darn gcd

--- homework.js
+++ homework.js
@@ -1,26 +1,53 @@
 // 1. Set the variable `givenName` to the string "Addison".
-
+        let givenName = "Addison"
 // 2. Set candies equal to 20, people to 6, and leftover equal
 // to the remainder of dividing 20 by 6.
-
+        let candies = "20"
+        let people = "6"
+        let leftover = 20 % 6
 // 3. Create a function called greeting that returns "Hello, <name>!",
 // where <name> is the name given as an argument to the function.
-
+        function greeting(name) {
+            return "Hello, " + name + "!"
+        }
 // 4. Create a function called isOdd that, given a number, will
 // return true if it is odd and false if it is not. An odd number is a
 // number which, when divided by 2, has a remainder of 1 or -1.
-
+       function isOdd(number) {
+        if ((number % 2 === 1 || number % 2 === -1)) {
+            return true
+            }
+        else {
+                return false}
+            }
+    
+        
+        
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not. An even number is a
 // number which, when divided by 2, has a remainder of 0.
-
+          function isEven(number) {
+              if (number % 2 === 0) {
+                  return true
+              }
+              else {
+                  return false
+              }
+          }  
 // 6. Create a function called fahrenheitToCelsius that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Celsius.
+          function fahrenheitToCelsius(temp) {
+              return ((temp-32) * (5/9))
+          }
+
 
 // 6. Create a function called celsiusToFahrenheit that takes a
 // Celsius temperature as an argument and returns the
 // temperature in Fahrenheit.
+          function celsiusToFahrenheit(temp) {
+              return (temp * (9/5) + 32)
+          }
 
 // 7. Create a function called fahrenheitToKelvin that takes a
 // Fahrenheit temperature as an argument and returns the
@@ -28,11 +55,21 @@
 // fahrenheitToCelsius function.
 // Absolute zero (0 K) is equivalent to −273.15 C.
 // 1 degree Kelvin equals 1 degree Celsius.
+          function fahrenheitToKelvin(temp) {
+              return ((temp -32) * (5/9) + 273.15)
+          }
 
 // 8. Create a function called lesser that takes two numbers as
 // arguments and returns the lesser of them. This function should
 // use an if/else statement.
-
+          function lesser(num1,num2) {
+              if ((num1 < num2)) {
+                  return num1
+              } else {
+                  return num2}}
+              
+          
+          
 // 9. Create a function called multigreeting that takes a name
 // and a language code and returns a version of "Hello, <name>!"
 // in the specified language. The supported languages and their
@@ -44,10 +81,26 @@
 // eo - Saluton, <name>!
 //
 // If any other language code is used, return nothing.
-
-// 10. The greatest common divisor (https://en.wikipedia.org/wiki/Greatest_common_divisor)
-// is the largest integer that, given two other integers, can be divided into them. For
-// example, the greatest common divisor of 24 and 81 is 3. The greatest common divisor of
+                function multigreeting(name,language) {
+                    if ((language === 'en')) {
+                        return "Hello, " + name + "!"
+                    }
+                    if ((language === 'es')) {
+                        return "¡Hola, " + name + "!"
+                    }
+                    if ((language === 'fr')) {
+                        return "Bonjour, " + name + "!"
+                    }
+                    if ((language === 'eo')) {
+                        return "Saluton, " + name + "!"
+                    }
+                    else {
+                        return
+                    }
+                }
+// 10. The greatest common divisor (https://en.wikipedia.org/wiki/Greatest_commonivisor)
+// is the largest integethat, given two other integers, can be divided into them. For
+// example, the greatest commdivisor of 24 and 81 is 3. The grtest common divisor of
 // 10 and 25 is 5.
 //
 // One method of calculating the greatest common divisor is the "binary GCD algorithm."
@@ -71,3 +124,20 @@
 
 // Write a function called gcd that takes two arguments and returns the greatest common
 // divisor using the instructions above.
+                function gcd(num1,num2) {
+                    if (num1 === num 2 && num1 % 2 === 0 && num2 % 2 === 0) {
+                        return num1 / 2
+                    }
+                    if (num 1 !== num 2) {
+                        return num1 / 2
+                    }
+                    else if (num2 % 2 === 0) {
+                        return num2 / 2
+                    }
+                    else if ((num1 > num2)) {
+                        return (num1-num2) / 2
+                    }
+                    else {
+                        return (num2 -num1) / 2
+                    }
+                }
